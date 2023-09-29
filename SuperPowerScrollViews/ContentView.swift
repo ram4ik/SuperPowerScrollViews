6dcc327f585f5668456ec00ea72538d7e9459823
Ramill Ibragimov <146063206+ribragimov-mwb@users.noreply.github.com>
2023-09-29 09:55:19 +0300

    Initial Commit

---
+++ SuperPowerScrollViews/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SuperPowerScrollViews
+//
+//  Created by Ramill Ibragimov on 9/29/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SuperPowerScrollViews/SuperPowerScrollViewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuperPowerScrollViewsApp.swift
+//  SuperPowerScrollViews
+//
+//  Created by Ramill Ibragimov on 9/29/23.
+//
+
+import SwiftUI
+
+@main
+struct SuperPowerScrollViewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

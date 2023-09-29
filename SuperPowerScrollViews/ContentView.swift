b49ce65856f3ed31dbdc94a4600a755b015b11c2
Ramill Ibragimov <146063206+ribragimov-mwb@users.noreply.github.com>
2023-09-29 10:04:41 +0300

    init

--- SuperPowerScrollViews/ContentView.swift
+++ SuperPowerScrollViews/ContentView.swift
@@ -7,13 +7,34 @@
 
 import SwiftUI
 
+struct Item: Identifiable {
+    let id = UUID()
+    let color: Color
+}
+
+struct MockData {
+    static var items = [
+        Item(color: .teal),
+        Item(color: .orange),
+        Item(color: .yellow),
+        Item(color: .pink),
+        Item(color: .purple),
+        Item(color: .green),
+        Item(color: .gray),
+        Item(color: .red)
+    ]
+}
+
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ScrollView(.horizontal) {
+            HStack {
+                ForEach(MockData.items) { item in
+                    Circle()
+                        .frame(width: 100, height: 100)
+                        .foregroundStyle(item.color.gradient)
+                }
+            }
         }
         .padding()
     }
